40b04443dbf249b4ff0876fff3936c6721fd4d4d
Jesús Silos <jesus@testingprogram.com.mx>
2024-04-18 09:29:37 -0600

    Primer commit de calculadora
    Tarea 1 - Calculadora

---
+++ Ejercicio 1 - Calculadora/Operaciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class OperacionesMatematicas
+{
+
+    public static double SumarNumeros(double numeroUno, double numeroDos) => numeroUno + numeroDos;
+        
+    public static double RestarNumeros(double numeroUno, double numeroDos) => numeroUno - numeroDos;
+      
+    public static double MultiplicarNumeros(double numeroUno, double numeroDos) => numeroUno * numeroDos;
+
+    public static double DividirNumeros(double numeroUno, double numeroDos) => numeroUno / numeroDos;
+
+}
+
+internal class OperacionesDeValidacion {
+
+    public static void ValidaElNumero(string? valorDeLaConsola, ref double numero) {
+
+        bool esValidoElNumero = false;
+        while (!esValidoElNumero)
+        {
+            esValidoElNumero = double.TryParse(valorDeLaConsola, out numero);
+            if (!esValidoElNumero) Console.WriteLine($"El valor '{valorDeLaConsola}' que escribiste NO es válido. Escribe un número válido.");
+        }
+
+    }
+
+}
+
+
